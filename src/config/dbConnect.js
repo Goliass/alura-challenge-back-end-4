55a4b1b0635c59f091ba140c89a88742256feca5
Goliass <sauthier.elias@gmail.com>
2022-10-10 19:56:19 -0300

    carregar dados para conexão ao banco do arquivo .env (anteriormente carregava do .env.js)

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,9 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import mongoose from "mongoose";
-import env from "../../.env.js";
 
-const dbLoginData = env();
-
-mongoose.connect(`mongodb+srv://${dbLoginData.DBUSER}:${dbLoginData.DBPASS}@${dbLoginData.DBCLUSTER}.fjfyv.mongodb.net/${dbLoginData.DBNAME}`);
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBCLUSTER}.fjfyv.mongodb.net/${process.env.DBNAME}`);
 const db = mongoose.connection;
 
 export default db;
