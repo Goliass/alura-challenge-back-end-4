2c88dfbd1cbbfed0b9da809244f8dbf54c4e6043
Goliass <sauthier.elias@gmail.com>
2022-11-28 20:26:53 -0300

    task 'Autenticação': criação de middleware para tratamento customizado dos erros de autenticação com token JWT (Bearer) 'JsonWebTokenError' e 'TokenExpiredError', retornando http status error 401 (client error), ao invés do padrão 500 (internal server error)

--- src/auth/authMiddlewares.js
+++ src/auth/authMiddlewares.js
@@ -5,8 +5,6 @@ function localStrategy(req, res, next) {
     passport.authenticate(
       'local', { session: false },
       (error, user) => {
-        console.log(error);
-
         if (error && new RegExp("InvalidArgumentError").test(error.message)) {
           res.status(401).json({ error: error.message });
           return;
@@ -30,4 +28,34 @@ function localStrategy(req, res, next) {
   }
 }
 
-export { localStrategy };
+function bearerStrategy(req, res, next) {
+  try {
+    passport.authenticate(
+      'bearer', { session: false },
+      (error, user) => {
+        console.log(error);
+
+        if (error && (new RegExp("JsonWebTokenError").test(error.name) || new RegExp("TokenExpiredError").test(error.name))) {
+          res.status(401).json({ error: error.message });
+          return;
+        }
+  
+        if (error) {
+          return res.status(500).json({ error: error.message });
+        }
+  
+        if (!user) {
+          return res.status(401).json();
+        }
+  
+        req.user = user;
+        return next();
+      }
+    )(req, res, next);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+export { localStrategy, bearerStrategy };
