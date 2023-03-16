5a3d2b12699d5a0978dfd395b4c3bb6b96ac3c11
Goliass <18060247+Goliass@users.noreply.github.com>
2023-03-16 00:36:44 -0300

    logout: configuração e conexão ao servidor remoto Redis/host 'redis-18070.c61.us-east-1-3.ec2.cloud.redislabs.com', considerando variáveis de ambiente (arq .env); alteração para a conexão ao servidor local Redis (localhost) também considerar variáveis de ambiente

--- redis/jwtBlacklist.js
+++ redis/jwtBlacklist.js
@@ -1,8 +1,26 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { createClient } from 'redis';
 
-const jwtBlacklistRedisClient = createClient({ prefix: 'jwt-blacklist:' });
-jwtBlacklistRedisClient.on('error', error => console.log('Redis Client Error', error));
+let creatClientOptions = {
+  prefix: 'jwt-blacklist:'
+}
+
+if (process.env.REDIS_PASS) { 
+  creatClientOptions.url = `redis://default:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+} else { // localhost
+  creatClientOptions.socket = {
+    host: process.env.REDIS_HOST,
+    port: process.env.PORT
+  }
+}
+
+const jwtBlacklistRedisClient = createClient(creatClientOptions);
+
+jwtBlacklistRedisClient.on('error', error => console.log('In-memory database connection error', error));
 
 await jwtBlacklistRedisClient.connect();
+console.log("In-memory database connection succeed");
 
 export { jwtBlacklistRedisClient };
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from './src/app.js';
 const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
-  console.log(`Servidor escutando na porta ${port}`);
+  console.log(`Application server listening on port ${port}`);
 })
