683d77d401dd6cfa91417d0be414b35040cc2a29
Goliass <18060247+Goliass@users.noreply.github.com>
2023-08-01 22:42:50 -0300

    atualização da URL de conexão ao servidor do banco de dados MongoDB (começou a ocorrer erro com a URL anteriormente funcional)

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,7 +3,8 @@ dotenv.config();
 
 import mongoose from "mongoose";
 
-mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBCLUSTER}.${process.env.SUBDOMAIN}.mongodb.net/${process.env.DBNAME}`);
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBNAME}.${process.env.SUBDOMAIN}.mongodb.net`);
+
 const db = mongoose.connection;
 
 export default db;
