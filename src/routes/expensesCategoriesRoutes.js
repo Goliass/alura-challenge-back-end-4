f6d349eeb32576b97ef74e233f926a64d66ace44
Goliass <18060247+Goliass@users.noreply.github.com>
2023-08-28 23:10:28 -0300

    validation of the expensesCategories/* routes parameters

--- src/routes/expensesCategoriesRoutes.js
+++ src/routes/expensesCategoriesRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 
 import ExpensesCategoriesController from "../controllers/expensesCategoriesController.js";
+
+import { reqParams, paramsValidation } from "../validators/expensesCategoriesValidator.js";
+
 import * as authMiddlewares from "../auth/authMiddlewares.js";
 
 const expensesCategoriesRouter = express.Router();
 
 expensesCategoriesRouter.get('/expensesCategories', authMiddlewares.bearerStrategy, ExpensesCategoriesController.list);
-expensesCategoriesRouter.post('/expensesCategories', authMiddlewares.bearerStrategy, ExpensesCategoriesController.add);
+
+expensesCategoriesRouter.post('/expensesCategories',
+  authMiddlewares.bearerStrategy,
+  paramsValidation(reqParams.description),
+  ExpensesCategoriesController.add);
 
 export default expensesCategoriesRouter;
