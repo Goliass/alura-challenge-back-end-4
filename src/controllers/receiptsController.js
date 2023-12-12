e4f08561e49f5b8b511435e33887129cd6111783
Goliass <18060247+Goliass@users.noreply.github.com>
2023-12-11 23:22:17 -0300

    inclusion of the /receipts/description/:description route; validation of the /receipts/* route parameters

--- src/controllers/receiptsController.js
+++ src/controllers/receiptsController.js
@@ -1,37 +1,41 @@
 import receipts from "../models/Receipt.js";
 
 class ReceiptsController {
+  static list = (req, res) => {
+    receipts.find((error, receipts) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Error retrieving receipts" });
+      }
+      return res.status(200).json(receipts);
+    });
+  };
 
-  static list = (req, res) => { 
-    const descriptionQuery = req.query.description;
+  static findByDescription = (req, res) => {
+    const descriptionParam = req.params.description;
 
-    if (descriptionQuery) {
-      receipts.find({description: {$regex: descriptionQuery, $options: 'i'}})
-        .exec((error, receipts) => {
-          if (error) {
-            console.log(error);
-            return res.status(500).json({message: 'Error retrieving receipts by query'});
-          }
-          return res.status(200).json(receipts);
-        });
-    } else {
-      receipts.find((error, receipts) => {
+    receipts
+      .find({ description: { $regex: descriptionParam, $options: "i" } })
+      .exec((error, receipts) => {
         if (error) {
           console.log(error);
-          return res.status(500).json({message: 'Error retrieving receipts'});
+          return res
+            .status(500)
+            .json({ message: "Error retrieving receipts by query" });
         }
         return res.status(200).json(receipts);
       });
-    }
   };
-  
+
   static findById = (req, res) => {
     const id = req.params.id;
-  
+
     receipts.findById(id, (error, receipts) => {
       if (error) {
         console.log(error);
-        return res.status(500).send({message: `${error.message} - Error finding receipt ${id}`});
+        return res
+          .status(500)
+          .send({ message: `${error.message} - Error finding receipt ${id}` });
       } else {
         return res.status(200).send(receipts);
       }
@@ -39,88 +43,93 @@ class ReceiptsController {
   };
 
   static findByYearAndMonth = (req, res) => {
-    const {year, month} = req.params;
+    const { year, month } = req.params;
 
-    if (year.length != 4) {
-      return res.status(400).json({message: 'year must have 4 digits'});
-    }
-    
-    receipts.find({
+    receipts
+      .find({
         $expr: {
           $and: [
-            { "$eq": [{"$year": "$date"}, year] },
-            { "$eq": [{"$month": "$date"}, month] }
-          ]
-        }
+            { $eq: [{ $year: "$date" }, year] },
+            { $eq: [{ $month: "$date" }, month] },
+          ],
+        },
       })
       .exec((error, receipts) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).json({message: 'Error retrieving receipts by year and month'});
-      }
-      return res.status(200).json(receipts);
-    });
+        if (error) {
+          console.log(error);
+          return res
+            .status(500)
+            .json({ message: "Error retrieving receipts by year and month" });
+        }
+        return res.status(200).json(receipts);
+      });
   };
 
-  
   static add = (req, res) => {
     let body = req.body;
-    body.description = body.description.toUpperCase(); 
-  
+    body.description = body.description.toUpperCase();
+
     let receipt = new receipts(body);
-  
+
     receipt.save((error) => {
       if (error) {
         console.log(error);
-        return res.status(500).send({message: `${error.message} - Error registering receipt`});
+        return res
+          .status(500)
+          .send({ message: `${error.message} - Error registering receipt` });
       } else {
         return res.status(201).send(receipt.toJSON());
-      }
-    })
-  };
-  
-  
-  static update = (req, res) => {
-    const id = req.params.id;
-    const body = req.body;
-  
-    if ((body && Object.keys(body).length === 0 && Object.getPrototypeOf(body) === Object.prototype) ||
-      ('description' in body && !body.description) || 
-      ('value' in body && !body.value) || 
-      ('date' in body && !body.date)) {
-      return res.status(400).send({message: 'Invalid body'});
-    }
-  
-    body.description = body.description.toUpperCase();
-  
-    receipts.findByIdAndUpdate(id, {$set: body}, (error, model) => {
-      
-      if (error) {
-        console.log(error);
-        return res.status(500).send({message: error.message});
-      } else if (!model) {
-        return res.status(404).send();
-      } else {
-        return res.status(200).send({message: `Receipt ${id} sucessfully updated`});
-      }
-    });
-  };
-  
-  static delete = (req, res) => {
-    const id = req.params.id;
-  
-    receipts.findByIdAndDelete(id, (error, model) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send({message: error.message});
-      } else if (!model) {
-        return res.status(404).send();
-      } else {
-        return res.status(200).send({message: `Receipt ${id} removed`});
       }
     });
   };
 
+  static update = (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+
+    if (
+      (body &&
+        Object.keys(body).length === 0 &&
+        Object.getPrototypeOf(body) === Object.prototype) ||
+      ("description" in body && !body.description) ||
+      ("value" in body && !body.value) ||
+      ("date" in body && !body.date)
+    ) {
+      return res.status(400).send({ message: "Invalid body" });
+    }
+
+    if ("description" in body) {
+      body.description = body.description.toUpperCase();
+    }
+
+    receipts.findByIdAndUpdate(id, { $set: body }, (error, model) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({ message: error.message });
+      } else if (!model) {
+        return res.status(404).send();
+      } else {
+        return res
+          .status(200)
+          .send({ message: `Receipt ${id} sucessfully updated` });
+      }
+    });
+  };
+
+  static delete = (req, res) => {
+    const id = req.params.id;
+
+    receipts.findByIdAndDelete(id, (error, model) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({ message: error.message });
+      } else if (!model) {
+        return res.status(404).send();
+      } else {
+        return res.status(200).send({ message: `Receipt ${id} removed` });
+      }
+    });
+  };
 }
 
 export default ReceiptsController;
